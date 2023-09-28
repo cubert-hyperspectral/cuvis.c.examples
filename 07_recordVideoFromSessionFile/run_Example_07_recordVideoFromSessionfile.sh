ecc05a54cd9f28c157870300f4c05aeab26baf82
Samuel Canadi <canadi@cubert-gmbh.de>
2023-09-28 12:00:59 +0200

    example 7 video

--- 07_recordVideoFromSessionFile/run_Example_07_recordVideoFromSessionfile.sh
+++ 07_recordVideoFromSessionFile/run_Example_07_recordVideoFromSessionfile.sh
@@ -3,7 +3,7 @@
 # Usage:
 # example app path 				[string] E.g. ./06_recordVideo.exe for windows or ./06_recordVideo for linux	
 # user settings directory 		[string] Default is ../../../sample_data/set_examples/settings
-# sessionfile		 			[string] Default is ../../../sample_data/set_examples/set0_single/single.cu3s
+# sessionfile		 			[string] Default is ../../../sample_data/set_examples/set1_video/video.cu3s
 # name of recording	directory	[string] The name of your output folder realtive to the example app path
 # exposure time 				[int   ] exposure (integration) time in ms 
 # auto exposure					[bool  ] 1 is "on", 0 is "off"; if "on" the aquisition will start with the given exposure time and then adjust on the fly 
@@ -13,7 +13,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ##########LINUX##########
 	./07_recordVideoFromSessionfile											\
 	"../../../sample_data/set_examples/settings" 							\
-	"../../../sample_data/set_examples/set0_single/single.cu3s"		\
+	"../../../sample_data/set_examples/set1_video/video.cu3s"		\
 	"video01"																\
 	100																		\
 	0																		\
@@ -23,7 +23,7 @@ else
     ##########WINDOWS##########
 	./07_recordVideoFromSessionfile.exe 									\
 	"../../../sample_data/set_examples/settings"							\
-	"../../../sample_data/set_examples/set0_single/single.cu3s"		\
+	"../../../sample_data/set_examples/set1_video/video.cu3s"		\
 	"video01"																\
 	100																		\
 	0																		\

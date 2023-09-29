e1d2b66db9d2f52330fc75711ab2968c6883717d
Simon Birkholz <birkholz@cubert-gmbh.de>
2023-09-29 14:30:55 +0200

    updated path of userplugin in new example data

--- 03_exportMeasurement/run_Example_03_exportMeasurement.sh
+++ 03_exportMeasurement/run_Example_03_exportMeasurement.sh
@@ -4,7 +4,7 @@
 # example app path 				[string] E.g. ./03_exportMeasurement.exe for windows or ./03_exportMeasurement for linux
 # user settings directory 		[string] Default is ../../../sample_data/set_examples/settings
 # sessionfile (.cu3s)			[string] Default is ../../../sample_data/set_examples/set0_single/single.cu3s
-# user plugin file (.xml) 		[string] Default is ../../../sample_data/set_examples/userplugin/cai.xml
+# user plugin file (.xml) 		[string] Default is ../../../sample_data/set_examples/plugin/ref/cai.xml
 # name of export directory 		[string] The name of your output folder realtive to the example app path
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -12,7 +12,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	./03_exportMeasurement												\
 	"../../../sample_data/set_examples/settings"						\
 	"../../../sample_data/set_examples/set0_single/single.cu3s" 		\
-	"../../../sample_data/set_examples/userplugin/cai.xml"				\
+	"../../../sample_data/set_examples/plugin/ref/cai.xml"				\
 	"export"															\
 
 else
@@ -20,7 +20,7 @@ else
 	./03_exportMeasurement.exe 											\
 	"../../../sample_data/set_examples/settings"						\
 	"../../../sample_data/set_examples/set0_single/single.cu3s" 		\
-	"../../../sample_data/set_examples/userplugin/cai.xml"				\
+	"../../../sample_data/set_examples/plugin/ref/cai.xml"				\
 	"export"															\
 
 fi
